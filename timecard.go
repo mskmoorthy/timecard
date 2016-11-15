78dc988829e334da0dfcccac9f518811b1d8455c
albshin <albshin07@gmail.com>
2016-11-15 02:05:58 -0500

    reorganized folder. added routing

--- timecard.go
+++ timecard.go
@@ -3,11 +3,20 @@ package main
 import (
   "log"
   "net/http"
+
+  "github.com/julienschmidt/httprouter"
 )
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+  http.ServeFile(w, r, "templates/index.html")
+}
+
 func main() {
-  fs := http.FileServer(http.Dir("static"))
-  http.Handle("/", fs)
+  router := httprouter.New()
+  router.GET("/", Index)
+
+  http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+  http.Handle("/", router)
 
   log.Println("Listening...")
   http.ListenAndServe(":3000", nil)
