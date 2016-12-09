334dac9e05a0fb54b812cbd88a83dc0242423e5f
albshin <albshin07@gmail.com>
2016-12-08 19:29:49 -0500

    time changes

--- app/controller/index.go
+++ app/controller/index.go
@@ -3,17 +3,37 @@ package controller
 import (
     "net/http"
     "html/template"
+    "time"
+
     "github.com/julienschmidt/httprouter"
 )
 
 type Page struct {
     Title string
     Days []int
-    Hours []int
+    Timestamps []string
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    p := Page{Title: "home", Days: []int{1,2,3,4,5,6,7}, Hours: []int{8,9,10,11,12,1,2,3,4,5,6,7,8,9,10}}
+    starthour := 8
+    endhour := 23
+    //intervals := 30
+
+    slots := make([]string, 0)
+    thetime := time.Date(2016,12,8,starthour,0,0,0,time.UTC)
+
+    mins := 0
+    for hr := starthour; hr < endhour; {
+        slots = append(slots, thetime.Format("1504"))
+        thetime = thetime.Add(30 * time.Minute)
+        mins += 30
+        if mins == 60 {
+            mins = 0
+            hr++
+        }
+    }
+
+    p := Page{Title: "home", Days: []int{1,2,3,4,5,6,7}, Timestamps: slots}
     thetemplate := template.Must(template.ParseFiles("templates/days.html", "templates/layout.html"))
     err := thetemplate.ExecuteTemplate(w, "layout", &p)
 
